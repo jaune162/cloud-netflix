c0ea13607eb20516cbecb6380d821523d6a4e777
Wang Chengwei <jaune162@126.com>
2017-04-25 22:02:26 +0800

    spring cloud config client

---
+++ spring-cloud-config-client/src/main/java/com/codestd/springcloud/client/Application.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Jaune on 2017/4/25.
+ */
+
+package com.codestd.springcloud.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${remote.url}")
+    private String remoteUrl;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/name")
+    public String getName() {
+        return this.name;
+    }
+
+    @RequestMapping("/remote")
+    public String remoteUrl() {
+        return this.remoteUrl;
+    }
+}
